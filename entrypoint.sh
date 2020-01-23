caa5c1484037fdca1ad8eac8aa318d77a08b0ddc
Alessandro Vozza <alessandro.vozza@microsoft.com>
2020-01-23 16:49:03 +0300

    xx

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,8 @@ INPUT_TAG=${INPUT_TAG:-$GITHUB_SHA}
 INPUT_REPOSITORY=${INPUT_REPOSITORY}
 INPUT_BRANCH=${INPUT_BRANCH:-master}
 
-echo "Building Docker image ${repository}/${INPUT_IMAGE}:${INPUT_TAG} from ${GITHUB_REPOSITORY} on ${INPUT_BRANCH} and using context ${INPUT_FOLDER} ; and pushing it to ${registry} Azure Container Registry"
-env
+echo "Building Docker image ${INPUT_REPOSITORY}/${INPUT_IMAGE}:${INPUT_TAG} from ${GITHUB_REPOSITORY} on ${INPUT_BRANCH} and using context ${INPUT_FOLDER} ; and pushing it to ${INPUT_REGISTRY} Azure Container Registry"
 
 az login --service-principal -u ${INPUT_SERVICE_PRINCIPAL} -p ${INPUT_SERVICE_PRINCIPAL_PASSWORD} --tenant ${INPUT_TENANT}
-#az login --service-principal -u $INPUT_SERVICE_PRINCIPAL -p $service_principal_password --tenant $tenant
 
-az acr build -r ${registry} -f ${INPUT_DOCKERFILE} -t ${INPUT_REPOSITORY}/${INPUT_IMAGE}:${INPUT_TAG} ${INPUT_GITHUB_REPOSITORY}#${INPUT_BRANCH}:${INPUT_FOLDER}
+az acr build -r ${INPUT_REGISTRY} -f ${INPUT_DOCKERFILE} -t ${INPUT_REPOSITORY}/${INPUT_IMAGE}:${INPUT_TAG} ${INPUT_GITHUB_REPOSITORY}#${INPUT_BRANCH}:${INPUT_FOLDER}
