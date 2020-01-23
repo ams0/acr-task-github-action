38ac90fef3672b0a38ead27ecf264fb00c039f01
Alessandro Vozza <alessandro.vozza@microsoft.com>
2020-01-23 16:42:16 +0300

    brackets and bukld

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,7 @@ INPUT_BRANCH=${INPUT_BRANCH:-master}
 echo "Building Docker image ${repository}/${INPUT_IMAGE}:${INPUT_TAG} from ${GITHUB_REPOSITORY} on ${INPUT_BRANCH} and using context ${INPUT_FOLDER} ; and pushing it to ${registry} Azure Container Registry"
 env
 
-az login --service-principal -u $INPUT_SERVICE_PRINCIPAL -p $service_principal_password --tenant $tenant
+az login --service-principal -u ${INPUT_SERVICE_PRINCIPAL} -p ${INPUT_SERVICE_PRINCIPAL_PASSWORD} --tenant ${INPUT_TENANT}
+#az login --service-principal -u $INPUT_SERVICE_PRINCIPAL -p $service_principal_password --tenant $tenant
 
 az acr build -r ${registry} -f ${INPUT_DOCKERFILE} -t ${INPUT_REPOSITORY}/${INPUT_IMAGE}:${INPUT_TAG} ${INPUT_GITHUB_REPOSITORY}#${INPUT_BRANCH}:${INPUT_FOLDER}
