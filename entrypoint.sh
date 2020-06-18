b5bbe9ed4524f09cef15e7e88e36c1ea9e95516e
Alessandro Vozza <alessandro.vozza@microsoft.com>
2020-06-18 16:24:38 +0200

    -n instead of -z

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,10 +10,14 @@ if [ "$INPUT_IMAGE" != "" ]; then
         IMAGE_PART="/${INPUT_IMAGE}"
 fi
 
-if [ -z "$INPUT_GIT_ACCESS_TOKEN" ]; then
+if [ -n "$INPUT_GIT_ACCESS_TOKEN" ]; then
         GIT_ACCESS_TOKEN_FLAG="${INPUT_GIT_ACCESS_TOKEN}@"
 fi
 
 echo "Building Docker image ${INPUT_REPOSITORY}${IMAGE_PART}:${INPUT_TAG} from ${GITHUB_REPOSITORY} on ${INPUT_BRANCH} and using context ${INPUT_FOLDER} ; and pushing it to ${INPUT_REGISTRY} Azure Container Registry"
+
+echo "Logging into azure.."
 az login --service-principal -u ${INPUT_SERVICE_PRINCIPAL} -p ${INPUT_SERVICE_PRINCIPAL_PASSWORD} --tenant ${INPUT_TENANT}
+
+echo "Sending build job to ACR.."
 az acr build -r ${INPUT_REGISTRY} -f ${INPUT_DOCKERFILE} -t ${INPUT_REPOSITORY}${IMAGE_PART}:${INPUT_TAG} https://${GIT_ACCESS_TOKEN_FLAG}github.com/${GITHUB_REPOSITORY}.git#${INPUT_BRANCH}:${INPUT_FOLDER}
