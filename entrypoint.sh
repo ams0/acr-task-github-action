9729f810589fdc71b65d6556d52ae977026c4902
Alessandro Vozza <alessandro.vozza@microsoft.com>
2020-06-18 16:01:04 +0200

    back to IMAGE_PART

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,11 @@ set -e
 INPUT_DOCKERFILE=${INPUT_DOCKERFILE:-Dockerfile}
 INPUT_TAG=${INPUT_TAG:-${GITHUB_SHA::8}}
 INPUT_BRANCH=${INPUT_BRANCH:-master}
+IMAGE_PART=""
+
+if [ "$INPUT_IMAGE" != "" ]; then
+        IMAGE_PART="/${INPUT_IMAGE}"
+fi
 
 if [ -z "$INPUT_GIT_ACCESS_TOKEN" ]; then
         GIT_ACCESS_TOKEN_FLAG="${INPUT_GIT_ACCESS_TOKEN}@"
@@ -11,4 +16,4 @@ fi
 
 echo "Building Docker image ${INPUT_REPOSITORY}${IMAGE_PART}:${INPUT_TAG} from ${GITHUB_REPOSITORY} on ${INPUT_BRANCH} and using context ${INPUT_FOLDER} ; and pushing it to ${INPUT_REGISTRY} Azure Container Registry"
 az login --service-principal -u ${INPUT_SERVICE_PRINCIPAL} -p ${INPUT_SERVICE_PRINCIPAL_PASSWORD} --tenant ${INPUT_TENANT}
-az acr build -r ${INPUT_REGISTRY} -f ${INPUT_DOCKERFILE} -t ${INPUT_REPOSITORY}/${INPUT_IMAGE}:${INPUT_TAG} https://${GIT_ACCESS_TOKEN_FLAG}github.com/${GITHUB_REPOSITORY}.git#${INPUT_BRANCH}:${INPUT_FOLDER}
+az acr build -r ${INPUT_REGISTRY} -f ${INPUT_DOCKERFILE} -t ${INPUT_REPOSITORY}${IMAGE_PART}:${INPUT_TAG} https://${GIT_ACCESS_TOKEN_FLAG}github.com/${GITHUB_REPOSITORY}.git#${INPUT_BRANCH}:${INPUT_FOLDER}
