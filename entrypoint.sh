c79060cdae1053504856ba41d9304ae279e19964
frodehus <frode.hus@outlook.com>
2020-08-31 12:43:30 +0200

    fixed naming on input variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ INPUT_DOCKERFILE=${INPUT_DOCKERFILE:-Dockerfile}
 INPUT_TAG=${INPUT_TAG:-${GITHUB_SHA::8}}
 INPUT_BRANCH=${INPUT_BRANCH:-master}
 IMAGE_PART=""
-if [ -n "$BUILD_ARGS" ]; then
-        DOCKER_BUILD_ARGS=`echo -n ${BUILD_ARGS:-''} |jq -j '.[] | keys[] as $k | values[] as $v |  "--build-arg \($k)=\"\($v)\" "'`
+if [ -n "$INPUT_BUILD_ARGS" ]; then
+        BUILD_ARGS=`echo -n ${INPUT_BUILD_ARGS:-''} |jq -j '.[] | keys[] as $k | values[] as $v |  "--build-arg \($k)=\"\($v)\" "'`
 fi
 
 if [ "$INPUT_IMAGE" != "" ]; then
@@ -16,11 +16,11 @@ fi
 if [ -n "$INPUT_GIT_ACCESS_TOKEN" ]; then
         GIT_ACCESS_TOKEN_FLAG="${INPUT_GIT_ACCESS_TOKEN}@"
 fi
-echo ${DOCKER_BUILD_ARGS}
+
 echo "Building Docker image ${INPUT_REPOSITORY}${IMAGE_PART}:${INPUT_TAG} from ${GITHUB_REPOSITORY} on ${INPUT_BRANCH} and using context ${INPUT_FOLDER} ; and pushing it to ${INPUT_REGISTRY} Azure Container Registry"
 
 echo "Logging into azure.."
 az login --service-principal -u ${INPUT_SERVICE_PRINCIPAL} -p ${INPUT_SERVICE_PRINCIPAL_PASSWORD} --tenant ${INPUT_TENANT}
 
 echo "Sending build job to ACR.."
-az acr build -r ${INPUT_REGISTRY} ${DOCKER_BUILD_ARGS} -f ${INPUT_DOCKERFILE} -t ${INPUT_REPOSITORY}${IMAGE_PART}:${INPUT_TAG} https://${GIT_ACCESS_TOKEN_FLAG}github.com/${GITHUB_REPOSITORY}.git#${INPUT_BRANCH}:${INPUT_FOLDER}
+az acr build -r ${INPUT_REGISTRY} ${BUILD_ARGS} -f ${INPUT_DOCKERFILE} -t ${INPUT_REPOSITORY}${IMAGE_PART}:${INPUT_TAG} https://${GIT_ACCESS_TOKEN_FLAG}github.com/${GITHUB_REPOSITORY}.git#${INPUT_BRANCH}:${INPUT_FOLDER}
